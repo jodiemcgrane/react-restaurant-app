e9895e1474096198a0caf289d6bda01040cfc4d0
jodiemcgrane <48021738+jodiemcgrane@users.noreply.github.com>
2021-11-23 18:03:32 +0000

    Mapped through the restaurants array created using UseState for React & outputting them in the Restaurants Index page

--- src/pages/restaurants/Index.js
+++ src/pages/restaurants/Index.js
@@ -2,21 +2,36 @@ import axios from "axios";
 import { useEffect, useState } from 'react'
 
 const Index = () => {
+    const [restaurants, setRestaurants] = useState(null)
 
     useEffect(() => {
         axios.get('http://localhost:8000/restaurants')
-        .then(response => {
-            console.log(response.data.restaurants)
-        })
-        .catch(err => {
-            console.log(`Error: ${err}`)
-        })
+            .then(response => {
+                console.log(response.data.restaurants)
+                setRestaurants(response.data.restaurants)
+            })
+            .catch(err => {
+                console.log(`Error: ${err}`)
+            })
+    }, [])
+
+    if (!restaurants) return null
+
+    const restaurantsList = restaurants.map(restaurant => {
+        return (
+            <div>
+                <p><b>Name: </b> {restaurant.name} </p>
+                <p><b>Cuisine: </b> {restaurant.cuisine} </p>
+                <hr />
+            </div>
+        )
     })
 
     return (
         <div>
             <h2>Restaurants</h2>
             <p>This is the Restaurants Index page</p>
+            {restaurantsList}
         </div>
     );
 }
