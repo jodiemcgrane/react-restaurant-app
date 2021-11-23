f48bde0c0f000a8f7365a2d4873530241c193c3b
jodiemcgrane <48021738+jodiemcgrane@users.noreply.github.com>
2021-11-23 17:49:16 +0000

    Able to log GET restaurants data to the console

--- src/pages/restaurants/Index.js
+++ src/pages/restaurants/Index.js
@@ -2,6 +2,17 @@ import axios from "axios";
 import { useEffect, useState } from 'react'
 
 const Index = () => {
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/restaurants')
+        .then(response => {
+            console.log(response.data.restaurants)
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`)
+        })
+    })
+
     return (
         <div>
             <h2>Restaurants</h2>
