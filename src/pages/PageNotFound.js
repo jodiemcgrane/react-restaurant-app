3add08c64d3d2188f60042bc0a298b24f5f0fff9
jodiemcgrane <48021738+jodiemcgrane@users.noreply.github.com>
2021-12-11 19:19:50 +0000

    Designed page not found component

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,16 +1,52 @@
 import { useLocation } from 'react-router-dom'
 
-//MUI
+//Styles
+import * as styles from '../styles/pagenotfound.module.css';
 
+//MUI
+import {
+    Grid,
+    Box,
+    Stack,
+    TextField,
+    Button,
+    Typography,
+    Card,
+    CardContent
+} from '@mui/material';
 
 const PageNotFound = () => {
 
     let location = useLocation();
 
     return (
-        <div>
-            <p>Soz page not found :( {location.pathname} wasnt found</p>
-        </div>
+        <Card className={styles.background} sx={{ boxShadow: 1 }}>
+            <CardContent className={styles.content}>
+                <Grid>
+                    <Box>
+                        <Stack
+                            direction="column"
+                            justifyContent="space-around"
+                            alignItems="center"
+                            spacing={3}
+                        >
+                            <Typography variant="h1">
+                                404 :(
+                            </Typography>
+
+                            <Typography variant="h2">Oops!</Typography>
+
+                            <Typography variant="h4">We could not find the page "{location.pathname}"</Typography>
+
+                            <Button href="/" variant="contained" color="secondary" size="large">
+                                Back To Home
+                            </Button>
+                        </Stack>
+
+                    </Box>
+                </Grid>
+            </CardContent>
+        </Card >
     );
 }
 
